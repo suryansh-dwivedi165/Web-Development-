9389c9d54913418c626d67a8cefef78da2b943a8
= <suryanshdwivedi39@gmail.com>
2026-04-29 23:40:34 +0530

    message

---
+++ js1/Traffic_light.js
@@ -0,0 +1,9 @@
+Signal_input = "Red"
+
+// Nested if else statement 
+if(Signal_input === "Red")
+    console.log("Stop!")
+else if(Signal_input === "Yellow")
+    console.log("Slow down") 
+else if(Signal_input === "Green") 
+    console.log("Go")           
