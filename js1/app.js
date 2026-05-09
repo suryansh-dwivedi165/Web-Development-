30e67db2ad76b1fa81d4d579da9250c3c48f3803
= <suryanshdwivedi39@gmail.com>
2026-05-09 23:47:07 +0530

    New change

---
+++ js1/app.js
@@ -0,0 +1,3 @@
+const data = require("./index.js"); 
+
+console.log(data); 
---
+++ js1/index.js
@@ -0,0 +1,4 @@
+module.exports.sum = (a, b) => a + b;
+module.exports.sub = (a, b) => a - b;
+module.exports.g = 9.8;
+module.exports.pi = 3.14; 
